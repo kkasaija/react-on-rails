cc045e0ade97849e591ef3f982f5767480e7f5c9
Kasaija Kenneth <kasaijak@gmail.com>
2023-03-01 18:28:32 -0800

    Render application to client side

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+  Message.create(greeting: "Greeting# #{i + 1}!")
+end
